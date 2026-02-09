05ba44bb8e221297e34ebe074e684615136d4bec
vangerald-sys <airahmarie671@gmail.com>
2026-02-09 18:11:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files (if you use them)
+python manage.py collectstatic --noinput
+
+# Run migrations to create your database tables
+python manage.py migrate
