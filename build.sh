481e7abb74a7d6309087e11e8215f6b17b32ece9
vangerald-sys <airahmarie671@gmail.com>
2026-02-09 18:22:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+# This line installs gunicorn!
 pip install -r requirements.txt
 
-# Collect static files (if you use them)
 python manage.py collectstatic --noinput
-
-# Run migrations to create your database tables
 python manage.py migrate
